bfe21fc4b686df141feaf18001baec67cbf34a8d
Bobbimon <tobbe_lobbe88@hotmail.com>
2016-04-11 22:42:06 +0200

    Added some lines in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
-// my second program in C++
+// my first program in C++
 #include <iostream>
+
+using namespace std;
 
 int main ()
 {
-    std::cout << "Hello World! ";
-    std::cout << "I'm a C++ program";
+    cout << "Hello World! " << endl;
+    cout << "I'm a C++ program" << endl;
+    return 0;
 }
