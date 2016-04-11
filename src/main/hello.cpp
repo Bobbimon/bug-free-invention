88bab3b29f6c82c22527b5ec5ae427501d278d08
Bobbimon <tobbe_lobbe88@hotmail.com>
2016-04-11 23:15:33 +0200

    Changed folders stucture and deleted the test.txt file

---
+++ src/main/hello.cpp
@@ -0,0 +1,13 @@
+// my first program in C++
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World! " << endl;
+    cout << "I'm a C++ program" << endl;
+    cout << "I'm a C++ asdasd" << endl;
+
+    return 0;
+}
