1d24dc2257ffe97e472f7ff8a3e19fea3558d3bb
Jon Granberg <jongranberg@hotmail.se>
2016-04-14 16:03:56 +0200

    Tjo faderittan
    hej hej
    :D

---
+++ src/main/AttackData.h
@@ -0,0 +1,21 @@
+/*
+ * AttackData.h
+ *
+ *  Created on: 14 apr. 2016
+ *      Author: jogr
+ */
+
+#ifndef SRC_MAIN_ATTACKDATA_H_
+#define SRC_MAIN_ATTACKDATA_H_
+
+class AttackData {
+public:
+	void setHit(int);
+	int getHit();
+	void setBlock(int);
+	int getBlock();
+	int hit;
+	int block;
+};
+
+#endif /* SRC_MAIN_ATTACKDATA_H_ */
--- src/main/hello.cpp
+++ src/main/hello.cpp
@@ -1,13 +1,25 @@
 // Our first program in C++
+#include "AttackData.h"
 #include <iostream>
 
 using namespace std;
 
+void attack();
+
 int main ()
 {
-    cout << "Hello World! " << endl;
-    cout << "I'm a C++ program" << endl;
-    cout << "I'm a C++ asdasd" << endl;
-
+	attack();
     return 0;
+}
+void attack(){
+	AttackData oh;
+	oh.setHit(20);
+
+
+	cout << "Character " << "      " << " attack "<<"      "<<" on hit  "<<"on block"<< endl;
+	cout << "Sabrewulf " << "------" << " attack "<<"------"<<"   "<<oh.getHit()<<" "<<"     -2"<< endl;
+	cout << "Jago      " << "------" << " attack "<<"------"<<"   3  "<<"     -2"<< endl;
+	cout << "Orchid    " << "------" << " attack "<<"------"<<"   3  "<<"     -2"<< endl;
+	cout << "Fulgore   " << "------" << " attack "<<"------"<<"   3  "<<"     -2"<< endl;
+
 }
